827f303102b009afcdf1bf9bcba5b908f286ef9b
ushnish399 <nilanjanchakraborty5@gmail.com>
2020-01-06 16:57:04 +0530

    made cars

--- sketch.js
+++ sketch.js
@@ -8,9 +8,13 @@ var database;
 
 var form, player, game;
 
+var car1, car2, car3, car4;
+
+var cars;
+
 
 function setup(){
-  canvas = createCanvas(400,400);
+  canvas = createCanvas(displayWidth-20,displayHeight-30);
   database = firebase.database();
   game = new Game();
   game.getState();
